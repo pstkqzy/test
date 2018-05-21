c029c4d0cc360081d82cb13ca8d6a16a305bf498
ZhangShaoPeng <284383884@qq.com>
2018-05-21 11:41:08 +0800

    init
     nothing

---
+++ Github-Test/Github-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github-Test
+//
+//  Created by TaoShang on 2018/5/21.
+//  Copyright © 2018年 TaoShang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github-Test/Github-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github-Test
+//
+//  Created by TaoShang on 2018/5/21.
+//  Copyright © 2018年 TaoShang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
